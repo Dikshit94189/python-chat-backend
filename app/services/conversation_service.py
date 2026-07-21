bf5d46d4f817ea1ea9af09aecd677183bd20c3a9
Dikshit Sharma <noreply@feedback.github.com>
2026-07-21 21:00:35 +0530

    add conservation service code

---
+++ app/services/conversation_service.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+
+from app.repositories.conversation_repo import ConversationRepository
+
+class ConversationService:
+
+    def __init__(self):
+        self.repository  = ConversationRepository()
+
+    def create_conversation(self, db:Session):
+        return self.repository.create_conversation(db)
+
+    def get_conversation_by_id(self, db:Session , conversation_id: int):
+        return self.repository.get_conversation_by_id(id, conversation_id)
+
+    def get_all_conversations(self, db:Session):
+        return self.repository.get_all_conversations(db)        
